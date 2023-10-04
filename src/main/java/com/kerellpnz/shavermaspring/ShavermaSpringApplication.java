d6209c63969a6ff39f2a06f4834cef31baa3ef59
k.noskov <k.noskov@lancelot-it.ru>
2023-10-04 23:12:16 +0300

    Initial commit

---
+++ src/main/java/com/kerellpnz/shavermaspring/ShavermaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kerellpnz.shavermaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShavermaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShavermaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kerellpnz/shavermaspring/ShavermaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerellpnz.shavermaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShavermaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
